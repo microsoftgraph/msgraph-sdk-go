0033a78585ff4efa0c1f85f528097431ea35d3a8
Omri Avigdor <avgdorom@users.noreply.github.com>
2022-02-17 18:34:12 +0200

    Fixing the Delta struct
    This struct is missing some parts that are being received as part of the response.
    I matched it to `UsersResponse` and added `deltaLink` (not sure if the latter is entirely correct).

--- users/delta/delta.go
+++ users/delta/delta.go
@@ -2,12 +2,19 @@ package delta
 
 import (
     i04eb5309aeaafadd28374d79c8471df9b267510b4dc2e3144c378c50f6fd7b55 "github.com/microsoft/kiota/abstractions/go/serialization"
+    i4a838ef194e4c99e9f2c63ba10dab9cb120a89367c1d4ab0daa63bb424e20d87 "github.com/microsoftgraph/msgraph-sdk-go/models/microsoft/graph"
 )
 
 // Delta 
 type Delta struct {
     // Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
     additionalData map[string]interface{};
+    //
+	nextLink *string;
+	//
+	deltaLink *string;
+	//
+	value []i4a838ef194e4c99e9f2c63ba10dab9cb120a89367c1d4ab0daa63bb424e20d87.User;
 }
 // NewDelta instantiates a new delta and sets the default values.
 func NewDelta()(*Delta) {
