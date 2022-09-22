1a71705530b4580095af8b3c31b77a141ffb761f
rkodev <43806892+rkodev@users.noreply.github.com>
2022-09-22 09:36:58 +0300

    Simply client initialization

--- graph_base_service_client.go
+++ graph_base_service_client.go
@@ -1,1 +1,21 @@
 package msgraphsdkgo
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	az "github.com/microsoft/kiota-authentication-azure-go"
+)
+
+func NewGraphServiceClientWithCredentials(credential azcore.TokenCredential, scopes []string) (*GraphServiceClient, error) {
+	auth, err := az.NewAzureIdentityAuthenticationProviderWithScopes(credential, scopes)
+	if err != nil {
+		return nil, err
+	}
+
+	adapter, err := NewGraphRequestAdapter(auth)
+	if err != nil {
+		return nil, err
+	}
+
+	client := NewGraphServiceClient(adapter)
+	return client, nil
+}
