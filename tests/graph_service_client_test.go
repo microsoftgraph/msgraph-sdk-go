5f728475e1342951967b33380e05bad534d9f126
rkodev <43806892+rkodev@users.noreply.github.com>
2022-09-22 21:39:18 +0300

    Move tests to separate folder

--- tests/graph_service_client_test.go
+++ tests/graph_service_client_test.go
@@ -1,9 +1,10 @@
-package msgraphsdkgo
+package tests
 
 import (
 	"context"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/microsoftgraph/msgraph-sdk-go"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -26,21 +27,21 @@ func (m *SimpleCredentials) GetToken(context.Context, policy.TokenRequestOptions
 
 func TestNewGraphServiceClientWithCredentialsInitializesAClient(t *testing.T) {
 	cred := NewSimpleCredentials("asdasd")
-	client, _ := NewGraphServiceClientWithCredentials(cred, []string{"Files.Read"})
+	client, _ := msgraphsdkgo.NewGraphServiceClientWithCredentials(cred, []string{"Files.Read"})
 
 	assert.NotNil(t, client)
 }
 
 func TestNewGraphServiceClientWithCredentialsAndHostsInitializesAClient(t *testing.T) {
 	cred := NewSimpleCredentials("asdasd")
-	client, _ := NewGraphServiceClientWithCredentialsAndHosts(cred, []string{"Files.Read"}, []string{"host1", "host2"})
+	client, _ := msgraphsdkgo.NewGraphServiceClientWithCredentialsAndHosts(cred, []string{"Files.Read"}, []string{"host1", "host2"})
 
 	assert.NotNil(t, client)
 }
 
 func TestClientAccessToGeneratedMethods(t *testing.T) {
 	cred := NewSimpleCredentials("asdasd")
-	client, _ := NewGraphServiceClientWithCredentials(cred, []string{"Files.Read"})
+	client, _ := msgraphsdkgo.NewGraphServiceClientWithCredentials(cred, []string{"Files.Read"})
 
 	assert.NotNil(t, client.Groups())
 }
