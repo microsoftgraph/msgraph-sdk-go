484dd80ae39109e7e2a212a393f08275afee49cd
Abhishek Singh <abhishek@nightfall.ai>
2023-06-24 18:05:48 +0530

    Add support for pagination in groups api

--- groups/groups_request_builder.go
+++ groups/groups_request_builder.go
@@ -29,6 +29,8 @@ type GroupsRequestBuilderGetQueryParameters struct {
     Skip *int32 `uriparametername:"%24skip"`
     // Show only the first n items
     Top *int32 `uriparametername:"%24top"`
+
+    SkipToken *string `uriparametername:"%24skiptoken"`
 }
 // GroupsRequestBuilderGetRequestConfiguration configuration for the request such as headers, query parameters, and middleware options.
 type GroupsRequestBuilderGetRequestConfiguration struct {
@@ -60,7 +62,7 @@ func (m *GroupsRequestBuilder) ByGroupId(groupId string)(*GroupItemRequestBuilde
 // NewGroupsRequestBuilderInternal instantiates a new GroupsRequestBuilder and sets the default values.
 func NewGroupsRequestBuilderInternal(pathParameters map[string]string, requestAdapter i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.RequestAdapter)(*GroupsRequestBuilder) {
     m := &GroupsRequestBuilder{
-        BaseRequestBuilder: *i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.NewBaseRequestBuilder(requestAdapter, "{+baseurl}/groups{?%24top,%24skip,%24search,%24filter,%24count,%24orderby,%24select,%24expand}", pathParameters),
+        BaseRequestBuilder: *i2ae4187f7daee263371cb1c977df639813ab50ffa529013b7437480d1ec0158f.NewBaseRequestBuilder(requestAdapter, "{+baseurl}/groups{?%24top,%24skip,%24search,%24filter,%24count,%24orderby,%24select,%24expand,%24skiptoken}", pathParameters),
     }
     return m
 }
